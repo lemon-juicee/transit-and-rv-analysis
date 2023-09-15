931afb21edec99a2f351939adaf2e7b0a499d336
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-09-15 04:12:59 -0700

    Write docstring for data_from_csv

--- tools.py
+++ tools.py
@@ -3,6 +3,7 @@ import pandas
 from pathlib import Path
 
 def data_from_csv(name):
+    '''Import a csv file from the data folder'''
     folder = Path("./data/")
     file = folder / name
     data = np.array(pandas.read_csv(file))
