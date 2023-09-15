aefabfaa215a9cc9e3def806d575abeb8e6d9888
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-09-15 11:39:18 -0700

    Define hist_comparison()

--- analysis.py
+++ analysis.py
@@ -5,3 +5,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import tools
 
+def hist_comparison(data1, data2, bins, range):
+    plt.hist(data1, range = range, color = 'red', bins = bins, alpha = 0.5, weights=np.ones_like(data1) / np.size(data1))
+    plt.hist(data2, range = range, color = 'blue', bins = bins, alpha = 0.5, weights=np.ones_like(data2) / np.size(data2))
