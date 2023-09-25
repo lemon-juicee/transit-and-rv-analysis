e38fff78d7b3d8af93043fa2140c961c8057a825
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-09-24 18:27:05 -0700

    Import data

--- workspace.py
+++ workspace.py
@@ -6,3 +6,7 @@ import seaborn as sns
 from scipy import stats
 import tools
 import analysis
+
+transit = tools.data_from_csv('m=transit;d=transit;mass=m-r.csv')
+rv = tools.data_from_csv('m=rv;d=rv;mass=msini.csv')
+trv = tools.data_from_csv('m=transit;d=rv,transit;mass=mass.csv')
