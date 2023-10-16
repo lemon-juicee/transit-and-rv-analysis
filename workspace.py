6b142bac5b084397a14e2be89bc7646607468472
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-10-15 22:53:53 -0700

    Test p-value method

--- workspace.py
+++ workspace.py
@@ -16,3 +16,9 @@ for planet in tools.data_from_csv('m=rv;d=rv;mass=msini.csv'):
 trv = [] 
 for planet in tools.data_from_csv('m=transit;d=rv,transit;mass=mass.csv'):
     trv.append(planet[1])
+
+p_dist = analysis.t_test_repeated(rv, trv, 50000, 80)
+print(np.median(p_dist))
+print(stats.ttest_ind(rv, trv, equal_var=False))
+analysis.histo(p_dist, 100, (0, 1), 'blue')
+plt.show()
