48b8e06728c3493034761f9389198a74ce7fb80d
lemon <111851880+lemon-juicee@users.noreply.github.com>
2023-10-17 16:02:35 -0700

    Define outlier_test()

--- tools.py
+++ tools.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas
 from pathlib import Path
+from scipy import stats
 
 def data_from_csv(name):
     '''Import a csv file from the data folder'''
@@ -8,3 +9,14 @@ def data_from_csv(name):
     file = folder / name
     data = np.array(pandas.read_csv(file))
     return data
+
+def outlier_test(data):
+    outliers = []
+    median = np.median(data)
+    iqr = stats.iqr(data)
+    lower = np.percentile(data, 25) - (iqr * 1.5)
+    upper = np.percentile(data, 75) + (iqr * 1.5)
+    for value in data:
+        if value < lower or value > upper:
+            outliers.append(value)
+    return outliers
